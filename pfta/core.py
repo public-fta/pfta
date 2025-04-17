c7767e03f35c270dbfecbba62a5ae2c565616128
Conway <yawnoc@pm.me>
2025-04-18 02:14:59 +0800

    Rewrite parser to only parse lines

--- pfta/core.py
+++ pfta/core.py
@@ -8,9 +8,9 @@ Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
 This is free software with NO WARRANTY etc. etc., see LICENSE.
 """
 
-from pfta.parser import parse
+from pfta.parser import parse_lines
 
 
 class FaultTree:
     def __init__(self, fault_tree_text: str):
-        parse(fault_tree_text)
+        parse_lines(fault_tree_text)
--- pfta/parser.py
+++ pfta/parser.py
@@ -9,7 +9,15 @@ This is free software with NO WARRANTY etc. etc., see LICENSE.
 """
 
 import re
-from enum import Enum
+
+
+LINE_EXPLAINER = '\n'.join([
+    'A line must have one of the following forms:',
+    '    <class>: <identifier>  (an object declaration)',
+    '    - <key>: <value>       (a property setting)',
+    '    # <comment>            (a comment)',
+    '    <blank line>           (used before the next declarations)',
+])
 
 
 class FaultTreeTextException(Exception):
@@ -19,28 +27,10 @@ class FaultTreeTextException(Exception):
         self.explainer = explainer
 
 
-class LineType(Enum):
-    OBJECT_DECLARATION = 0
-    PROPERTY_SETTING = 1
-    BLANK_LINE = 2
-    COMMENT_LINE = 3
-
-
 class ParsedLine:
-    LINE_EXPLAINER = '\n'.join([
-        'A line must have one of the following forms:',
-        '    Event: <identifier>    (an Event declaration)',
-        '    Gate: <identifier>     (an Gate declaration)',
-        '    - <key>: <value>       (a property setting)',
-        '    # <comment>            (a comment)',
-        '    <blank line>           (used before the next declarations)',
-    ])
-    ID_EXPLAINER = 'An identifier must contain only ASCII letters, digits, underscores, and hyphens.'
-
-    def __init__(self, number: int, line: str, type_: LineType, info: dict):
+    def __init__(self, number: int, line: str, info: dict):
         self.number = number
         self.line = line
-        self.type_ = type_
         self.info = info
 
     class InvalidIdException(FaultTreeTextException):
@@ -50,35 +40,48 @@ class ParsedLine:
         pass
 
 
+class ObjectDeclarationLine(ParsedLine):
+    pass
+
+
+class PropertySettingLine(ParsedLine):
+    pass
+
+
+class CommentLine(ParsedLine):
+    pass
+
+
+class BlankLine(ParsedLine):
+    pass
+
+
+def is_valid_class(class_: str) -> bool:
+    return class_ in {'Event', 'Gate'}
+
+
 def is_valid_id(id_: str) -> bool:
     return bool(re.fullmatch('[a-z0-9_-]', id_, flags=re.IGNORECASE))
 
 
 def parse_line(line_number: int, line: str) -> ParsedLine:
-    if object_match := re.match(r'^(?P<class_>Event|Gate):\s+(?P<id_>.+?)\s*$', line):
-        class_ = object_match.group('class_')
-        id_ = object_match.group('id_')
-
-        if not is_valid_id(id_):  # TODO: move to enact_line
-            raise ParsedLine.InvalidIdException(line_number, f'invalid identifier `{id_}`', ParsedLine.ID_EXPLAINER)
-
-        return ParsedLine(line_number, line, LineType.OBJECT_DECLARATION, info={'class_': class_, 'id_': id_})
+    if object_match := re.match(r'^(?P<class_>\S+):\s+(?P<id_>.+?)\s*$', line):
+        return ObjectDeclarationLine(line_number, line, info=object_match.groupdict())
 
     if property_match := re.match(r'^- (?P<key>\S+):\s+(?P<value>.+?)\s*$', line):
-        key = property_match.group('key')
-        value = property_match.group('value')
+        return PropertySettingLine(line_number, line, info=property_match.groupdict())
 
-        return ParsedLine(line_number, line, LineType.PROPERTY_SETTING, info={'key': key, 'value': value})
+    if re.match('^\s*#.*$', line):  # comment match (allow whitespace)
+        return CommentLine(line_number, line, info={})
 
-    if re.match('^\s*#.*$', line):  # comment match
-        return ParsedLine(line_number, line, LineType.COMMENT_LINE, info={})
+    if re.match('^\s*$', line):  # blank line (allow whitespace)
+        return BlankLine(line_number, line, info={})
 
-    if re.match('^\s*$', line):  # blank line
-        return ParsedLine(line_number, line, LineType.BLANK_LINE, info={})
-
-    raise ParsedLine.InvalidLineException(line_number, f'invalid line `{line}`', ParsedLine.LINE_EXPLAINER)
+    raise ParsedLine.InvalidLineException(line_number, f'invalid line `{line}`', LINE_EXPLAINER)
 
 
-def parse(fault_tree_text: str):
-    for line_number, line in enumerate(fault_tree_text.splitlines(), start=1):
-        parsed_line = parse_line(line_number, line)
+def parse_lines(fault_tree_text: str) -> list[ParsedLine]:
+    return [
+        parse_line(line_number, line)
+        for line_number, line in enumerate(fault_tree_text.splitlines(), start=1)
+    ]
