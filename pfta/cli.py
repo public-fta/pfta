cfbcc1e66191e9174a5b487448275cd117ee6f0e
Conway <yawnoc@pm.me>
2025-04-18 00:02:19 +0800

    Implement fault tree text file argument

--- pfta/cli.py
+++ pfta/cli.py
@@ -9,6 +9,8 @@ This is free software with NO WARRANTY etc. etc., see LICENSE.
 """
 
 import argparse
+import os
+import shutil
 
 from pfta._version import __version__
 
@@ -20,12 +22,37 @@ def parse_cli_arguments():
         action='version',
         version=f'{parser.prog} version {__version__}',
     )
+    parser.add_argument(
+        'fault_tree_text_file',
+        type=argparse.FileType('r'),
+        help='fault tree text file; output is written to `{ft.txt}.out/`',
+        metavar='ft.txt',
+    )
 
     return parser.parse_args()
 
 
+def mkdir_robust(directory_name: str):
+    if os.path.isfile(directory_name):
+        os.remove(directory_name)
+
+    if os.path.isdir(directory_name):
+        shutil.rmtree(directory_name)
+
+    os.mkdir(directory_name)
+
+
 def main():
     arguments = parse_cli_arguments()
+    fault_tree_text_file = arguments.fault_tree_text_file
+
+    # TODO: main fault tree computation
+
+    output_directory_name = f'{fault_tree_text_file.name}.out'
+
+    mkdir_robust(output_directory_name)
+
+    # TODO: write output
 
 
 if __name__ == '__main__':
