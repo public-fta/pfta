9387636f9f42b5385f1392395d7f11ce590655b9
Conway <yawnoc@pm.me>
2025-04-23 23:42:48 +0800

    Prefer 'leading' and 'last' over 'penultimate'

--- pfta/common.py
+++ pfta/common.py
@@ -39,8 +39,9 @@ def natural_join(items: tuple | list, penultimate_separator: str | None = 'and')
     if length == 2:
         return f'{items[0]} {penultimate_separator} {items[1]}'
 
-    joined_to_penultimate = ', '.join(str(item) for item in items[:-1])
-    return f'{joined_to_penultimate}, {penultimate_separator} {items[-1]}'
+    leading_items_joined = ', '.join(str(item) for item in items[:-1])
+    last_item = items[-1]
+    return f'{leading_items_joined}, {penultimate_separator} {last_item}'
 
 
 def natural_join_backticks(items: tuple | list, penultimate_separator: str | None = 'and') -> str:
