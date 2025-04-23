4c25d4ed30d024a912a1d7c931c30d3b7d986eb1
Conway <yawnoc@pm.me>
2025-04-23 18:14:53 +0800

    Write ConstantDistribution

---
+++ pfta/prng.py
@@ -0,0 +1,22 @@
+"""
+# Public Fault Tree Analyser: prng.py
+
+Pseudo-random number generation.
+
+**Copyright 2025 Conway.**
+Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+This is free software with NO WARRANTY etc. etc., see LICENSE.
+"""
+
+
+class Distribution:
+    def generate_samples(self, count: int) -> list[float]:
+        raise NotImplementedError
+
+
+class ConstantDistribution(Distribution):
+    def __init__(self, value: float):
+        self.value = value
+
+    def generate_samples(self, count: int) -> list[float]:
+        return [self.value for _ in range(count)]
