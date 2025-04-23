aee3cf9214efeccfd2b6a55ae856e33f0016770a
Conway <yawnoc@pm.me>
2025-04-23 21:20:41 +0800

    Implement UniformDistribution

--- pfta/sampling.py
+++ pfta/sampling.py
@@ -8,6 +8,8 @@ Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
 This is free software with NO WARRANTY etc. etc., see LICENSE.
 """
 
+import random
+
 
 class Distribution:
     def generate_samples(self, count: int) -> list[float]:
@@ -20,3 +22,15 @@ class ConstantDistribution(Distribution):
 
     def generate_samples(self, count: int) -> list[float]:
         return [self.value for _ in range(count)]
+
+
+class UniformDistribution(Distribution):
+    def __init__(self, lower: float, upper: float):
+        self.lower = lower
+        self.upper = upper
+
+    def generate_samples(self, count: int) -> list[float]:
+        a = self.lower
+        b = self.upper
+
+        return [random.uniform(a, b) for _ in range(count)]
