20d319d90ac5e559462cd93ee3aeb0bf66404c4e
Conway <yawnoc@pm.me>
2025-04-23 12:07:13 +0800

    Write natural_join

--- pfta/common.py
+++ pfta/common.py
@@ -20,3 +20,19 @@ def natural_repr(self):
     class_name = type(self).__name__
     argument_sequence = ', '.join(f'{key}={value!r}' for key, value in self.__dict__.items())
     return f'{class_name}({argument_sequence})'
+
+
+def natural_join(items: list) -> str:
+    if not items:
+        return ''
+
+    length = len(items)
+
+    if length == 1:
+        return str(items[0])
+
+    if length == 2:
+        return f'{items[0]} and {items[1]}'
+
+    joined_to_penultimate = ', '.join(str(item) for item in items[0:-1])
+    return f'{joined_to_penultimate}, and {items[-1]}'
---
+++ tests/test_common.py
@@ -0,0 +1,33 @@
+"""
+# Public Fault Tree Analyser: test_common.py
+
+Unit testing for `common.py`.
+
+**Copyright 2025 Conway.**
+Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+This is free software with NO WARRANTY etc. etc., see LICENSE.
+"""
+
+import unittest
+
+from pfta.common import natural_join
+
+
+class TestCommon(unittest.TestCase):
+    def test_natural_join(self):
+        self.assertEqual(natural_join([]), '')
+
+        self.assertEqual(natural_join([1]), '1')
+        self.assertEqual(natural_join(['one']), 'one')
+
+        self.assertEqual(natural_join([1, 2]), '1 and 2')
+        self.assertEqual(natural_join(['one', 'two']), 'one and two')
+
+        self.assertEqual(natural_join([1, 2, 3]), '1, 2, and 3')
+        self.assertEqual(natural_join(['one', 'two', 'three']), 'one, two, and three')
+
+        self.assertEqual(natural_join([1, 2, 3, 4, 5, 6, 7, 8, 9]), '1, 2, 3, 4, 5, 6, 7, 8, and 9')
+        self.assertEqual(
+            natural_join(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']),
+            'one, two, three, four, five, six, seven, eight, and nine',
+        )
