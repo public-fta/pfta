3495e3fa7aeca16a454d405adc268105aee5f0ac
Conway <yawnoc@pm.me>
2025-04-25 01:46:41 +0800

    Revise constant_rate_model_probability edge case logic

--- pfta/computation.py
+++ pfta/computation.py
@@ -15,29 +15,52 @@ def constant_rate_model_probability(t: float, lambda_: float, mu: float) -> floa
     """
     Instantaneous failure probability q(t) for a component with constant failure and repair rates λ and μ.
 
-    Explicitly, q(t) = [λ/(λ+μ)] [1 − exp(−(λ+μ)t)].
+    Explicitly, q(t) = [λ/(λ+μ)] [1−exp(−(λ+μ)t)].
 
-    - If λ = 0 and μ = 0 and t = inf | nan,    then q(t) = nan  (since [...] and [...] are independently indeterminate)
-    - If λ = 0 and μ = 0 and t ≠ inf | nan,    then q(t) = 0    (from expanding in the small parameter (λ+μ)t)
-    - If λ = 0 and μ ≠ 0,                      then q(t) = 0    (since [...] = 0 and 0 ≤ [...] ≤ 1)
-    - If λ = inf and μ = inf | nan,            then q(t) = nan  (since [...] is indeterminate and 0 ≤ [...] ≤ 1)
-    - If λ = inf and μ ≠ inf | nan and t = 0,  then q(t) = nan  (since [...] = 1 and [...] is indeterminate)
-    - If λ = inf and μ ≠ inf | nan and t ≠ 0,  then q(t) = 1    (since [...] = 1 and [...] = 1)
-    - If λ = nan,                              then q(t) = nan  (since [...] and [...] are independently indeterminate)
-    - If λ ≠ 0 | inf | nan and μ = inf,        then q(t) = 0    (since [...] = 0 and 0 ≤ [...] ≤ 1)
-    - If λ ≠ 0 | inf | nan and μ = nan,        then q(t) = nan  (since [...] and [...] are independently indeterminate)
+    |  λ  |  μ  |  t  |  q  | Explanation
+    | --- | --- | --- | --- | -----------
+    |  0  |  0  | i|n | nan | 0/0 [1−exp(−0.i|n)] = nan (since 0/0 is independent of i|n)
+    |     |     | oth |  0  | λ/(λ+μ).(λ+μ)t = λt = 0
+    |     | inf | any |  0  | 0/i [1−exp(−i.any)] = 0.finite = 0
+    |     | nan | i|n | nan | {nan (per above) if μ=0}
+    |     |     | oth |  0  | {0 (per above) if μ=0; 0/μ [1−exp(−μt)] = 0.finite = 0 if μ≠0}  # mergeable with next
+    |     | oth | any |  0  | 0/μ [1−exp(−μ.any)] = 0.finite = 0
+    | inf | i|n | any | nan | i/(i+i|n) [1−exp(−(i+i|n).any)] = nan.finite = nan
+    |     | oth | 0|n | nan | 1 [1−exp(−inf.0|n)] = 1.nan = nan
+    |     |     | oth |  1  | 1 [1−exp(−inf.t)] = 1.1 = 1
+    | nan |  0  | i|n | nan | {nan (per above) if λ=0}
+    |     |     | oth | nan | 1 [1−exp(−nan.t)] = nan                                         # mergeable with previous
+    |     | i|n | any | nan | {nan (per above) if λ=inf}                                      # mergeable with previous
+    |     | oth | any | nan | nan [1−exp(−nan.any)] = nan.finite = nan                        # mergeable with previous
+    | oth | inf | any |  0  | λ/i [1−exp(−i.any)] = 0.finite = 0
+    |     | nan | inf | nan | {0 (per above) if μ=inf; 1 [1−exp(−λ.inf)] = 1 if μ=0}
+    |     |     | oth | nan | {0 (per above) if μ=inf; 1 [1−exp(−λ.t)] ≠ 0 if μ=0}            # mergeable with previous
+    |     | oth | any | :-) | computable
     """
     if lambda_ == 0:
-        if mu == 0 and (math.isinf(t) or math.isnan(t)):
-            return float('nan')
+        if mu == 0:
+            if math.isinf(t) or math.isnan(t):
+                return float('nan')
+
+            return 0.
+
+        if math.isinf(mu):
+            return 0.
+
+        if math.isnan(mu):
+            if math.isinf(t) or math.isnan(t):
+                return float('nan')
 
         return 0.
 
     if math.isinf(lambda_):
-        if not (math.isinf(mu) or math.isnan(mu)) and (t != 0):
-            return 1.
+        if math.isinf(mu) or math.isnan(mu):
+            return float('nan')
 
-        return float('nan')
+        if t == 0 or math.isnan(t):
+            return float('nan')
+
+        return 1.
 
     if math.isnan(lambda_):
         return float('nan')
---
+++ tests/test_computation.py
@@ -0,0 +1,115 @@
+"""
+# Public Fault Tree Analyser: test_computation.py
+
+Unit testing for `computation.py`.
+
+**Copyright 2025 Conway.**
+Licensed under the GNU General Public License v3.0 (GPL-3.0-only).
+This is free software with NO WARRANTY etc. etc., see LICENSE.
+"""
+
+import math
+import unittest
+
+from pfta.computation import constant_rate_model_probability
+
+INF = float('inf')
+NAN = float('nan')
+
+
+class TestComputation(unittest.TestCase):
+
+    def test_constant_rate_model_probability(self):
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=0, mu=0, t=INF)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=0, mu=0, t=NAN)))
+
+        self.assertEqual(constant_rate_model_probability(lambda_=0, mu=0, t=0), 0.)
+        self.assertEqual(constant_rate_model_probability(lambda_=0, mu=0, t=1), 0.)
+
+        self.assertEqual(constant_rate_model_probability(lambda_=0, mu=INF, t=INF), 0.)
+        self.assertEqual(constant_rate_model_probability(lambda_=0, mu=INF, t=NAN), 0.)
+        self.assertEqual(constant_rate_model_probability(lambda_=0, mu=INF, t=0), 0.)
+        self.assertEqual(constant_rate_model_probability(lambda_=0, mu=INF, t=1), 0.)
+
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=0, mu=NAN, t=INF)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=0, mu=NAN, t=NAN)))
+
+        self.assertEqual(constant_rate_model_probability(lambda_=0, mu=NAN, t=0), 0.)
+        self.assertEqual(constant_rate_model_probability(lambda_=0, mu=NAN, t=1), 0.)
+
+        self.assertEqual(constant_rate_model_probability(lambda_=0, mu=1, t=INF), 0.)
+        self.assertEqual(constant_rate_model_probability(lambda_=0, mu=1, t=NAN), 0.)
+        self.assertEqual(constant_rate_model_probability(lambda_=0, mu=1, t=0), 0.)
+        self.assertEqual(constant_rate_model_probability(lambda_=0, mu=1, t=1), 0.)
+
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=INF, mu=INF, t=INF)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=INF, mu=INF, t=NAN)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=INF, mu=INF, t=0)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=INF, mu=INF, t=1)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=INF, mu=NAN, t=INF)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=INF, mu=NAN, t=NAN)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=INF, mu=NAN, t=0)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=INF, mu=NAN, t=1)))
+
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=INF, mu=0, t=NAN)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=INF, mu=0, t=0)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=INF, mu=1, t=NAN)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=INF, mu=1, t=0)))
+
+        self.assertEqual(constant_rate_model_probability(lambda_=INF, mu=0, t=INF), 1.)
+        self.assertEqual(constant_rate_model_probability(lambda_=INF, mu=0, t=1), 1.)
+        self.assertEqual(constant_rate_model_probability(lambda_=INF, mu=1, t=INF), 1.)
+        self.assertEqual(constant_rate_model_probability(lambda_=INF, mu=1, t=1), 1.)
+
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=0, t=INF)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=0, t=NAN)))
+
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=0, t=0)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=0, t=1)))
+
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=INF, t=INF)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=INF, t=NAN)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=INF, t=0)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=INF, t=1)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=NAN, t=INF)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=NAN, t=NAN)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=NAN, t=0)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=NAN, t=1)))
+
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=1, t=INF)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=1, t=NAN)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=1, t=0)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=NAN, mu=1, t=1)))
+
+        self.assertEqual(constant_rate_model_probability(lambda_=1, mu=INF, t=INF), 0.)
+        self.assertEqual(constant_rate_model_probability(lambda_=1, mu=INF, t=NAN), 0.)
+        self.assertEqual(constant_rate_model_probability(lambda_=1, mu=INF, t=0), 0.)
+        self.assertEqual(constant_rate_model_probability(lambda_=1, mu=INF, t=1), 0.)
+
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=1, mu=NAN, t=INF)))
+
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=1, mu=NAN, t=NAN)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=1, mu=NAN, t=0)))
+        self.assertTrue(math.isnan(constant_rate_model_probability(lambda_=1, mu=NAN, t=1)))
+
+        self.assertTrue(
+            math.isclose(
+                constant_rate_model_probability(lambda_=1, mu=0, t=math.log(2)),
+                1/2,
+                rel_tol=1e-15,
+            ),
+        )
+        self.assertTrue(
+            math.isclose(
+                constant_rate_model_probability(lambda_=3, mu=4, t=5),
+                0.4285714285714283012092817079861691868990917608624530832108001585160503999788026,  # WolframAlpha
+                rel_tol=1e-15,
+            ),
+        )
+        self.assertTrue(
+            math.isclose(
+                constant_rate_model_probability(lambda_=1e-13, mu=1e-14, t=1),
+                9.99999999999945000000000002016666666666611208333333334553416666666644298472222e-14,  # WolframAlpha
+                rel_tol=1e-15,
+            ),
+        )
